72755c56f072dd1af796a17f30abb579ed7d2ce8
jessica-calderon <calderonjessica13@yahoo.com>
2022-07-03 20:12:28 -0500

    code refactor

--- __tests__/random.test.js
+++
@@ -1,6 +0,0 @@
-const randomNumber = require('../lib/random.js');
-
-test('checks if 10 is equal to 10', () => {
-    expect(randomNumber()).toBeGreaterThanOrEqual(1);
-    expect(randomNumber()).toBeLessThanOrEqual(10);
-});
